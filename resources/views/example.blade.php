3b5611497de458da3149e3e439851aafa5002109
Fathma Khatun Mim <fathmakhatunmim@gmail.com>
2025-11-25 11:27:01 +0600

    vite

--- resources/views/example.blade.php
+++ resources/views/example.blade.php
@@ -4,10 +4,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    @vite('resources/css/app.css','resources/js/app.js')
 </head>
 <body>
 
-<h1>Blade Template</h1>
+<h1 id="heading">Blade Template</h1>
 
 
     {!!"<h3>HI<h3>"!!}
@@ -21,15 +22,23 @@
 
     @php
     $user = "yahoo baba";
-    $name = ["mim","dim"];
+    $name = ["mim","dim","jim","vim"];
     @endphp
     {{$user}}
     <ul>
      @foreach ($name as $item)
 
         {{-- <li>{{$loop->iteration}}-{{$item}}</li> --}}
-         @if($loop->first)
+         {{-- @if($loop->first)
          <li style="color:red";>{{$item}}</li>
+         @else
+         <li>{{ $item }}</li>
+         @endif --}}
+
+           @if($loop->even)
+         <li style="color:red";>{{$item}}</li>
+          @elseif($loop->odd)
+         <li style="color:rgb(19, 177, 19)";>{{$item}}</li>
          @else
          <li>{{ $item }}</li>
          @endif
@@ -52,6 +61,17 @@
     <!-- javascript o amra ayvabe lekha thake -->
 
  {{-- {!!"<script>alert('yahoo baba')</script>"!!} --}}
+
+<p id="para">Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique veniam magni vel corrupti obcaecati nemo voluptas tenetur commodi ipsam sed esse, itaque placeat. Sunt nemo est earum accusamus doloremque maxime.</p>
+
+
+
+
+
+
+
+
+
 
 </body>
 </html>
